d93e6c8135fe5694e99a47003c37c9800c1163e3
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-03-31 03:23:41 +1000

    eventlistener added

--- app.js
+++ app.js
@@ -1,17 +1,54 @@
-const choices = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
+const choices = ["rock", "paper", "scissors"];
+
+const mainDiv = document.querySelector("#mainDiv");
+
+const rock = document.createElement("button");
+rock.id = "rock";
+rock.textContent = "rock";
+
+const paper = document.createElement("button");
+paper.textContent = "paper";
+paper.id = "paper";
+
+const scissors = document.createElement("button");
+scissors.textContent = "scissors";
+scissors.setAttribute("id", "scissors");
+
+const display = document.createElement("div");
+mainDiv.append(display);
+
+mainDiv.append(rock);
+mainDiv.append(paper);
+mainDiv.append(scissors);
+
+// select player for score display
+const playerDisplay = document.querySelector(".player");
+const comp = document.querySelector(".comp");
+
+// EVENT LISTENERS FOR ALL THREE BUTTONS
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    playerDisplay.textContent = `Player: ${e.target.textContent}`;
+  });
+});
+
+// FUNCTIONS
 
 // COMPUTER CHOICE
 const getComputerChoice = () => {
   return (compChoice = choices[Math.floor(Math.random() * choices.length)]);
 };
 
-const getPlayerChoice = () => {
-  const playerChoice = prompt("What is your weapon of choice?");
-  return playerChoice;
-};
+// Player CHOICE
+// const getPlayerChoice = () => {
+//   const playerChoice = prompt("What is your weapon of choice?");
+//   return playerChoice;
+// };
 
+// GAMEPLAY LOGIC
 const playRound = () => {
   const playerSelection = getPlayerChoice();
   const computerSelection = getComputerChoice();
@@ -38,9 +75,9 @@ const playRound = () => {
       console.log(`It's a tie! Both chose ${playerSelection}`);
   }
 };
-
-const game = () => {
-  for (let i = 0; i < 5; i++) {
-    playRound();
-  }
-};
+// LOOP
+// const game = () => {
+//   for (let i = 0; i < 5; i++) {
+//     playRound();
+//   }
+// };
